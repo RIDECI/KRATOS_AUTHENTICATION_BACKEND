e1efb32462ce93ab8bafa4e4c2f915630e8733d4
Dav10-32 <david.palacios-p@mail.escuelaing.edu.co>
2025-12-13 02:51:43 -0500

    fix: CORS

--- src/main/java/edu/dosw/rideci/infrastructure/config/SecurityConfig.java
+++ src/main/java/edu/dosw/rideci/infrastructure/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package edu.dosw.rideci.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Deshabilitamos CSRF porque usamos JWT
+                .cors(Customizer.withDefaults())                .csrf(csrf -> csrf.disable()) // Deshabilitamos CSRF porque usamos JWT
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
@@ -36,4 +37,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
